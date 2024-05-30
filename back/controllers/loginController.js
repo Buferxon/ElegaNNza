61dbb6f7ae0c0508a832e7a074834197716099b1
Robinson Buenaventura <112020206+Buferxon@users.noreply.github.com>
2024-05-30 16:33:01 -0500

    bloqueo usuarios inactivos

--- back/controllers/loginController.js
+++ back/controllers/loginController.js
@@ -20,8 +20,13 @@ async function logIn(req, res) {
 			password_hash: hashedPassword,
 		});
 
+		// console.log(result);
 
-		
+		if(result.status==0){
+			return res.status(401).json({ error: "Usuario Inactivo" });
+		}
+
+
 		if (result != null) {
 			const collectionItem = conexionDB.collection("item");
 			var expiresdTime = "1h";
@@ -33,11 +38,9 @@ async function logIn(req, res) {
 
 			if (type_userasdasd.description == "web") {
 				expiresdTime = "1h";
-
 			} else if (type_userasdasd.description == "movil") {
 				expiresdTime = "30d";
 			}
-			
 
 			const token = jwt.sign(
 				{
@@ -52,15 +55,13 @@ async function logIn(req, res) {
 
 			res.set("Access-Control-Expose-Headers", "Authorization");
 			res.setHeader("authorization", token);
-			res
-				.status(200)
-				.json({
-					name: result.name,
-					last_name: result.last_name,
-					user_name: result.user_name,
-					user_type: type_userasdasd.description,
-					authorization:token
-				});
+			res.status(200).json({
+				name: result.name,
+				last_name: result.last_name,
+				user_name: result.user_name,
+				user_type: type_userasdasd.description,
+				authorization: token,
+			});
 		} else {
 			res.status(401).json({ error: "Credenciales incorrectas" });
 		}
