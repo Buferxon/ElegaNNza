d01a0a0c5e97ba18bf5eea3a1228c636f8e38f89
Robinson Buenaventura <112020206+Buferxon@users.noreply.github.com>
2024-05-30 14:54:30 -0500

    import cambio

--- front/elegannza/app/[locale]/dashboard/page.jsx
+++ front/elegannza/app/[locale]/dashboard/page.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import Dashboard from './dashboard'
-import Sidebar from '../Components/Sidebar'
+import React from "react";
+import Dashboard from "./Dashboard";
+import Sidebar from "../Components/Sidebar";
 //import './page.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function page() {
-  return (
-    <>
-   <div>
-
-          <Dashboard />
-
-		</div>	
-    </>
-  )
+	return (
+		<>
+			<div>
+				<Dashboard />
+			</div>
+		</>
+	);
 }
 
-export default page
+export default page;
